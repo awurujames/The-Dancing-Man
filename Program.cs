e969ae8ef310dd100c1198f9361332631fa07799
Awuru James <mondayjamesawuru10@mail.com>
2020-10-12 23:10:29 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Dancing_Man
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("c_     ___");
+            Console.WriteLine("   \\__|__|__");
+            Console.WriteLine("    \\( o_o)");
+            Console.WriteLine("       > ~ >");
+            Console.WriteLine("      /    / \\\\ ");
+            Console.WriteLine("     /    /   \\\\ ");
+            Console.WriteLine("     \\   )   c/ ");
+            Console.WriteLine("     /  /");
+            Console.WriteLine("    /  /|");
+            Console.WriteLine("   (  ( \\");
+            Console.WriteLine("   |  |  \\");
+            Console.WriteLine("   | / \\  ) ");
+            Console.WriteLine("   | |  ) |");
+            Console.WriteLine("   /  )(_/");
+            Console.WriteLine("  (_ /");
+            
+            Console.WriteLine(" - The Dancing Man");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
